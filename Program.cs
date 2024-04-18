532de576df0dc2b4ffba0b957d8a7e8493996a2a
JONATA <105253719+JonataRubens@users.noreply.github.com>
2024-04-18 16:48:22 -0300

    up

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase:");
+        string input = Console.ReadLine();
+
+        Dictionary<string, int> frequenciaPalavras = ContarFrequenciaPalavras(input);
+
+        Console.WriteLine("\nFrequência de Palavras:");
+        foreach (var par in frequenciaPalavras)
+        {
+            Console.WriteLine($"{par.Key}: {par.Value} vezes");
+        }
+    }
+
+    static Dictionary<string, int> ContarFrequenciaPalavras(string input)
+    {
+        string[] palavras = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string, int> frequencia = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var palavra in palavras)
+        {
+            if (frequencia.ContainsKey(palavra))
+            {
+                frequencia[palavra]++;
+            }
+            else
+            {
+                frequencia[palavra] = 1;
+            }
+        }
+
+        return frequencia;
+    }
+}
